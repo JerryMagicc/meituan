1238544118869ee627d00b504031ffcaf84e118a
JerryMa <573754996@qq.com>
2018-08-31 15:21:11 +0800

    Third Commit

--- app/src/main/java/activitytest/example/com/mtapp/AddActivity.java
+++ app/src/main/java/activitytest/example/com/mtapp/AddActivity.java
@@ -5,10 +5,12 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import activitytest.example.com.mtapp.Adapter.ShopAdapter;
 import activitytest.example.com.mtapp.shiti.Shop;
+import cn.bmob.v3.Bmob;
 import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.FindListener;
@@ -18,7 +20,7 @@ import cn.bmob.v3.listener.FindListener;
  */
 
 public class AddActivity extends BaseActivity{
-    List<Shop> list;
+    List<Shop> list = new ArrayList<>();
     ListView listView;
     ShopAdapter shopAdapter;
 
@@ -41,19 +43,20 @@ public class AddActivity extends BaseActivity{
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
-        shopAdapter = new ShopAdapter(AddActivity.this,list);
+        shopAdapter = new ShopAdapter(this,list);
         loadData();
         listView = findViewById(R.id.list_add);
-
+        listView.setAdapter(shopAdapter);
     }
     private void loadData(){
-
+        Bmob.initialize(this, "9fa2d3643cd24762f417759a18bf4c1c");
         BmobQuery<Shop> bmobQuery = new BmobQuery<>();
         bmobQuery.findObjects(new FindListener<Shop>() {
             @Override
             public void done(final List<Shop> list, BmobException e) {
                 if (e == null){
-                    listView.setAdapter(new ShopAdapter(AddActivity.this,list));
+                   // listView.setAdapter(new ShopAdapter(AddActivity.this,list));
+                    shopAdapter.setShopList(list);
 
                 }else{
                     Log.i("bmob","失败："+e.getMessage()+","+e.getErrorCode());
